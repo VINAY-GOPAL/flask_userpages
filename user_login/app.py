ccc5e2f38eadc8b78cf58fb96a747fa95b210097
Vinay Gopal Dulam <vdulam@innominds.com>
2021-11-26 18:15:02 +0530

    Added forget password

--- user_login/app.py
+++ user_login/app.py
@@ -10,23 +10,33 @@ import bcrypt
 
 app = Flask(__name__)
 app.secret_key = "testing"
+#connection of mongodb through the pymongo
 client = pymongo.MongoClient("mongodb+srv://vinaygopal44:vinaygopal@cluster0.n4auo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client.get_database('total_records')
 records = db.register
 
+'''
+Description: using this index method we are storing the users data in the mongodb and authentication of the user.
 
+Params: fullname, email, password, repeat password
+
+Return: returns the index.html
+'''
 @app.route("/", methods=['post', 'get'])
 def index():
     message = ''
-    if "email" in session:
+    #if user login through email, session is started and running till the user logsout
+    if "email" in session:                      
         return redirect(url_for("logged_in"))
-    if request.method == "POST":
+    #send the data through the http POST method to the database 
+    if request.method == "POST":    
         user = request.form.get("fullname")
         email = request.form.get("email")
         
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
         
+        #Checking the user details in the database, if matches shows the appropriate message to the user
         user_found = records.find_one({"name": user})
         email_found = records.find_one({"email": email})
         if user_found:
@@ -38,6 +48,7 @@ def index():
         if password1 != password2:
             message = 'Passwords should match!'
             return render_template('index.html', message=message)
+            #encrypting the user password
         else:
             hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
             user_input = {'name': user, 'email': email, 'password': hashed}
@@ -50,6 +61,14 @@ def index():
     return render_template('index.html')
 
 
+
+'''
+Description:  Used to show the user email in the loggged_in
+
+Params: email
+
+Return: if logged_in else login components
+'''
 @app.route('/logged_in')
 def logged_in():
     if "email" in session:
@@ -59,6 +78,14 @@ def logged_in():
         return redirect(url_for("login"))
 
 
+
+'''
+Description:  Used to login in to the user account
+
+Params: email,password
+
+Return: login
+'''
 @app.route("/login", methods=["POST", "GET"])
 def login():
     message = 'Please login to your account'
@@ -90,7 +117,13 @@ def login():
 
 
 
+'''
+Description:  Used to logout the user account, session is terminated
 
+Params: email
+
+Return: index
+'''
 @app.route("/logout", methods=["POST", "GET"])
 def logout():
     if "email" in session:
@@ -98,6 +131,12 @@ def logout():
         return render_template("signout.html")
     else:
         return render_template('index.html')
+
+
+# @app.route("/reset_password", methods=["POST", "GET"])
+# def forgot_password():
+#     return render_template('forgetpassword.html')
+
 
 
 #end of code to run it
